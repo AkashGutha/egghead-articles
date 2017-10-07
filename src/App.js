50e7f98b9881f3d594b77941a708f3607c994cc4
Akash Gutha <akash.gutha@outlook.com>
2017-10-07 17:36:50 +0530

    stripping comments

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
-// Import routing components
 import { Route } from "react-router";
-// Import pages
 import LandingPage from "./Pages/LandingPage";
 
 class App extends Component {
