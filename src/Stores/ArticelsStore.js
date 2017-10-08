790ae067b51939249feebbf7e923f1c6018bfc1e
Akash Gutha <akash.gutha@outlook.com>
2017-10-08 10:39:46 +0530

    added simple article store

---
+++ src/Stores/ArticelsStore.js
@@ -0,0 +1,17 @@
+import { extendObservable } from "mobx";
+import ArticlesData from "./../Data/Articles.json";
+
+export default class ArticleStore {
+  constructor() {
+    extendObservable(this, {
+      articles: []
+    });
+  }
+
+  getArticles(options) {
+    if (options !== undefined) {
+    } else {
+      this.articles = ArticlesData;
+    }
+  }
+}
