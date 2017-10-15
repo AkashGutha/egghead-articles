f6e6d32545876c4864839b9d398921c7e3c47994
Akash Gutha <akash.gutha@outlook.com>
2017-10-15 12:16:30 +0530

    added scroll to top functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider, observer } from "mobx-react";
-import LandingPage from "./Pages/LandingPage";
 
+import ScrollToTop from "./Components/ScrollToTop";
+import LandingPage from "./Pages/LandingPage";
 import ArticleDetailPage from "./Pages/ArticleDetailPage";
+
 import Store from "./Stores/ArticleStore";
 import { loadArticles } from "./Services/ArticleService";
 
@@ -19,19 +21,26 @@ const App = observer(
       const routerProps = this.routerProps;
 
       return (
-        <Router onUpdate={() => window.scrollTo(0, 0)}>
-          <div>
-            <Route exact path="/">
-              <LandingPage store={Store} {...routerProps} />
-            </Route>
-            <Route
-              exact
-              path="/article/:id"
-              render={(props, article) => {
-                return <ArticleDetailPage store={Store} {...props} />;
-              }}
-            />
-          </div>
+        <Router
+          onUpdate={() => {
+            console.log("updated router");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <ScrollToTop>
+            <div>
+              <Route exact path="/">
+                <LandingPage store={Store} {...routerProps} />
+              </Route>
+              <Route
+                exact
+                path="/article/:id"
+                render={(props, article) => {
+                  return <ArticleDetailPage store={Store} {...props} />;
+                }}
+              />
+            </div>
+          </ScrollToTop>
         </Router>
       );
     }
